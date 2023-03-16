07548f419527e06f0c444648da21e1341ab6cbd2
steezy <karadimos.giannis@gmail.com>
2023-03-16 21:22:18 +0200

    added certificates

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 document.getElementById("show-more-btn").addEventListener("click", showMoreProjects);
 document.getElementById("show-less-btn").addEventListener("click", showLessProjects);
+document.getElementById("show-more-cert-btn").addEventListener("click", showMoreCertificates);
+document.getElementById("show-less-cert-btn").addEventListener("click", showLessCertificates);
 
 var showmore = document.getElementById("show-more-btn");
 var showless = document.getElementById("show-less-btn");
+var showmorecert = document.getElementById("show-more-cert-btn");
+var showlesscert = document.getElementById("show-less-cert-btn");
 
 function showMoreProjects() {
   var additionalProjects = document.getElementsByClassName("additional-project");
@@ -20,5 +24,23 @@ function showLessProjects() {
   }
   showmore.style.display = "block";
   showless.style.display = "none";
+}
+
+function showMoreCertificates() {
+  var additionalCertificates = document.getElementsByClassName("additional-certificate");
+  for (var i = 0; i < additionalCertificates.length; i++) {
+    additionalCertificates[i].style.display = "flex";
+  }
+  showmorecert.style.display = "none";
+  showlesscert.style.display = "block";
+}
+
+function showLessCertificates() {
+  var additionalCertificates = document.getElementsByClassName("additional-certificate");
+  for (var i = 0; i < additionalCertificates.length; i++) {
+    additionalCertificates[i].style.display = "none";
+  }
+  showmorecert.style.display = "block";
+  showlesscert.style.display = "none";
 }
 
